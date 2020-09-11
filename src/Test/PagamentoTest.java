f7a3fc58efa34a9f092669b63a8ed5bf7cfceeae
Diego Cravo Teixeira <cravo.diego@gmail.com>
2020-09-11 12:02:22 -0300

    - Pequeno ajuste

--- src/Test/PagamentoTest.java
+++ src/Test/PagamentoTest.java
@@ -7,17 +7,15 @@ import static org.junit.Assert.*;
 
 public class PagamentoTest {
 
-
+    Pagamento pagamento = new Pagamento(50, false);
 
     @Test
     public void testCalcularTroco() {
-        Pagamento pagamento = new Pagamento(50, true);
         assertEquals(50, pagamento.calcularTroco(100), 0.000000001);
     }
 
     @Test
     public void testRegistrarPagamento(){
-        Pagamento pagamento = new Pagamento(50, false);
         pagamento.registrarPagamento();
         assertTrue(pagamento.isPago());
     }
